38a46715eac6ebdd47ed44d563fbaeaef0a2bb82
Denzkristangacusan <gdenzkristan@gmail.com>
2024-11-04 18:28:00 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+
+let age = 30;
+// 
+if (age < 5) {
+    console.log("Denz (under 6 years)");
+} else if (age >= 6 && age <= 12) {
+    console.log("Child (6-12 years)");
+} else if (age >= 14 && age <= 19) {
+    console.log("Teenager (14-19 years)");
+} else if (age >= 25 && age <= 35) {
+    console.log("Young Adult (25-35 years)");
+} else if (age >= 40 && age <= 60) {
+    console.log("Middle-Aged (40-60 years)");
+} else {
+    console.log("Senior (over 60 years)");
+}
